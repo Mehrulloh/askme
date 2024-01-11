ecdb3d1c20e3e0a37dd33f796fe5d1ae18251de8
Mehrulloh Boboev <mehrullohboboev@Mehrullohs-MacBook-Air.local>
2024-01-11 07:59:14 +0100

    added session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user_params = params.require(:session)
+
+    user = User.find_by(email: session[:email])
+    if user.present?
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Вы вошли на сайт"
+    else
+      render :new, :alert => "Неправильно email или пароль!"
+    end
   end
 end
